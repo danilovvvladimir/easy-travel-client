3ba6856763133d425be111aa51eb8a905b5c257e
Vladimir <fruityflunt@gmail.com>
2024-04-26 06:54:48 +0300

    ET-CLIENT-7 Configure Storybook, Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,42 @@
 import { ButtonHTMLAttributes, FC } from 'react';
+import classNames from 'classnames';
+import styles from './Button.module.scss';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export enum ButtonKind {
+  Primary,
+  PrimaryInverted,
+  Secondary,
+  SecondaryInverted
+}
 
-const Button: FC<ButtonProps> = ({ className, children, ...restProps }) => {
-  return <button {...restProps}>{children}</button>;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  kind?: ButtonKind;
+  // TODO: сделать возможность создать button как Link
+}
+
+const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  kind = ButtonKind.Primary,
+  ...restProps
+}) => {
+  const finalClassName = classNames(
+    styles.button,
+    styles[kind],
+    {
+      [styles.disabled]: restProps.disabled
+    },
+    className
+  );
+
+  return (
+    <button
+      className={finalClassName}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 
 const HomePage: FC = () => {
   const { t } = useTranslation();
@@ -15,6 +16,7 @@ const HomePage: FC = () => {
       {t('title')}
       <button onClick={toggleLanguage}>123</button>
       <Link to="about">2</Link>
+      <Button kind="primary">111</Button>
     </div>
   );
 };
